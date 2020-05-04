bef58e1a42d66e867a4d92eb71969e26bd09068b
David Rhoden <david@davidrhoden.com>
2020-05-04 11:04:11 -0500

    change lightbox background

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
           type: 'image',
           delegate: 'a',
           gallery: {
-            enabled:true,
+            enabled: true,
             preload: [0,2],
             navigateByImgClick: true,
             arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
@@ -68,11 +68,11 @@ $(document).ready(function() {
         $('#results').html(data); 
         $('.grid-container').magnificPopup({
           type: 'image',
-          delegate: 'a',
+          delegate: 'a.photo-overlay',
           gallery: {
-            enabled:true,
+            enabled: true,
             preload: [0,2],
-            navigateByImgClick: true,
+            // navigateByImgClick: true,
             arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
             tPrev: 'Previous (Left arrow key)',
             tNext: 'Next (Right arrow key)',
@@ -95,34 +95,4 @@ $(document).ready(function() {
         });
     });
   });
-
-  $('.grid-container').magnificPopup({
-    type: 'image',
-    delegate: 'a',
-    image: {
-      markup: '<div class="mfp-figure">'+
-              '<div class="mfp-close"></div>'+
-              '<div class="mfp-img"></div>'+
-              '<div class="mfp-bottom-bar">'+
-              '<div class="mfp-title"></div>'+
-              '<div class="mfp-counter"></div>'+
-            '</div>'+
-          '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
-
-      cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
-
-      titleSrc: 'title', // Attribute of the target element that contains caption for the slide.
-      // Or the function that should return the title. For example:
-      // titleSrc: function(item) {
-      //   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-      // }
-
-      verticalFit: true, // Fits image in area vertically
-
-      tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
-        // other options
-      },
-      gallery:{enabled:true}
-    });
-
 });
