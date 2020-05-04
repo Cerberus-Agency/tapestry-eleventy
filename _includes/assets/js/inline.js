6762e6c46e78cbdd992e06741afcc278a16b29bb
David Rhoden <david@davidrhoden.com>
2020-05-04 12:37:48 -0500

    nothing really, code prettification

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,98 +1,97 @@
 if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
+    window.netlifyIdentity.on("init", user => {
+        if (!user) {
+            window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+            });
+        }
+    });
 }
 
 $(document).ready(function() {
-  var $hamburger = $(".hamburger");
-  $hamburger.on("click", function(e) {
-    $hamburger.toggleClass("is-active");
-    // Do something else, like open/close menu
-    $("#site-navigation").toggleClass("is-active");
-    $("#cls-1").toggleClass("is-active");
-  });
-
-  $("#contact-form").submit(function(e) {
-    e.preventDefault();
-
-    var $form = $(this);
-    $.post($form.attr("action"), $form.serialize()).then(function() {
-      alert("Thank you!");
+    var $hamburger = $(".hamburger");
+    $hamburger.on("click", function(e) {
+        $hamburger.toggleClass("is-active");
+        // Do something else, like open/close menu
+        $("#site-navigation").toggleClass("is-active");
+        $("#cls-1").toggleClass("is-active");
     });
-  });
 
-  $(document).on('change','#chair-covers', function(){
-    var category = $(this).val();
-    console.log(category);
-    $.get('/rentals/chair-covers/'+ category +'/index.html', function( data ) {
-        $('#results').html(data); 
-        $('.grid-container').magnificPopup({
-          type: 'image',
-          delegate: 'a',
-          gallery: {
-            enabled: true,
-            preload: [0,2],
-            navigateByImgClick: true,
-            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
-            tPrev: 'Previous (Left arrow key)',
-            tNext: 'Next (Right arrow key)',
-            tCounter: '<span class="mfp-counter">%curr% of %total%</span>'
-          },
-          image: {
-              markup: '<div class="mfp-figure">'+
-                      '<div class="mfp-close"></div>'+
-                      '<div class="mfp-img"></div>'+
-                      '<div class="mfp-bottom-bar">'+
-                      '<div class="mfp-title"></div>'+
-                      '<div class="mfp-counter"></div>'+
-                      '</div>'+
-                      '</div>',
-              cursor: 'mfp-zoom-out-cur', 
-              titleSrc: 'title', 
-              verticalFit: true,
-              tError: '<a href="%url%">The image</a> could not be loaded.'
-            }
+    $("#contact-form").submit(function(e) {
+        e.preventDefault();
+
+        var $form = $(this);
+        $.post($form.attr("action"), $form.serialize()).then(function() {
+            alert("Thank you!");
         });
     });
-  });
 
-    $(document).on('change','#table-linens', function(){
-    var category = $(this).val();
-    console.log(category);
-    $.get('/rentals/table-linens/'+ category +'/index.html', function( data ) {
-        $('#results').html(data); 
-        $('.grid-container').magnificPopup({
-          type: 'image',
-          delegate: 'a.photo-overlay',
-          gallery: {
-            enabled: true,
-            preload: [0,2],
-            // navigateByImgClick: true,
-            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
-            tPrev: 'Previous (Left arrow key)',
-            tNext: 'Next (Right arrow key)',
-            tCounter: '<span class="mfp-counter">%curr% of %total%</span>'
-          },
-          image: {
-              markup: '<div class="mfp-figure">'+
-                      '<div class="mfp-close"></div>'+
-                      '<div class="mfp-img"></div>'+
-                      '<div class="mfp-bottom-bar">'+
-                      '<div class="mfp-title"></div>'+
-                      '<div class="mfp-counter"></div>'+
-                      '</div>'+
-                      '</div>',
-              cursor: 'mfp-zoom-out-cur', 
-              titleSrc: 'title', 
-              verticalFit: true,
-              tError: '<a href="%url%">The image</a> could not be loaded.'
-            }
+    $(document).on('change', '#chair-covers', function() {
+        var category = $(this).val();
+        console.log(category);
+        $.get('/rentals/chair-covers/' + category + '/index.html', function(data) {
+            $('#results').html(data);
+            $('.grid-container').magnificPopup({
+                type: 'image',
+                delegate: 'a',
+                gallery: {
+                    enabled: true,
+                    preload: [0, 2],
+                    navigateByImgClick: true,
+                    arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
+                    tPrev: 'Previous (Left arrow key)',
+                    tNext: 'Next (Right arrow key)',
+                    tCounter: '<span class="mfp-counter">%curr% of %total%</span>'
+                },
+                image: {
+                    markup: '<div class="mfp-figure">' +
+                            '<div class="mfp-close"></div>' +
+                            '<div class="mfp-img"></div>' +
+                            '<div class="mfp-bottom-bar">' +
+                            '<div class="mfp-title"></div>' +
+                            '<div class="mfp-counter"></div>' +
+                            '</div>' +
+                            '</div>',
+                    titleSrc: 'title',
+                    verticalFit: true,
+                    tError: '<a href="%url%">The image</a> could not be loaded.'
+                }
+            });
         });
     });
-  });
+
+    $(document).on('change', '#table-linens', function() {
+        var category = $(this).val();
+        console.log(category);
+        $.get('/rentals/table-linens/' + category + '/index.html', function(data) {
+            $('#results').html(data);
+            $('.grid-container').each(function() {
+                $(this).magnificPopup({
+                    type: 'image',
+                    delegate: 'p a.photo-overlay',
+                    gallery: {
+                        enabled: true,
+                        preload: [0, 1],
+                        navigateByImgClick: true,
+                        arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
+                        tPrev: 'Previous (Left arrow key)',
+                        tNext: 'Next (Right arrow key)',
+                    },
+                    image: {
+                        markup: '<div class="mfp-figure">' +
+                                '<div class="mfp-close"></div>' +
+                                '<div class="mfp-img"></div>' +
+                                '<div class="mfp-bottom-bar">' +
+                                '<div class="mfp-title"></div>' +
+                                '<div class="mfp-counter"></div>' +
+                                '</div>' +
+                                '</div>',
+                        titleSrc: 'title',
+                        verticalFit: true,
+                        tError: '<a href="%url%">The image</a> could not be loaded.'
+                    }
+                });
+            });
+        });
+    });
 });
